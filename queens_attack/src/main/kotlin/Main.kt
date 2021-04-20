7711dfd7aade440678945a3038c5bfe03e4f27d0
hernanbosqued <hernanbosqued@gmail.com>
2021-04-20 20:17:20 -0300

    queens attack

--- queens_attack/src/main/kotlin/Main.kt
+++ queens_attack/src/main/kotlin/Main.kt
@@ -1,10 +1,58 @@
 package hernanbosqued.samples
 
+import java.lang.RuntimeException
 import java.util.*
 
 // Complete the queensAttack function below.
 fun queensAttack(n: Int, k: Int, r_q: Int, c_q: Int, obstacles: Array<Array<Int>>): Int {
-    return 0
+
+    val allObstacles = mutableSetOf<Pair<Int, Int>>()
+    allObstacles.addAll(obstacles.map { Pair(it[0], it[1]) })
+
+    if (allObstacles.contains(Pair(r_q, c_q))) {
+        throw RuntimeException("obstacle in queen cell")
+    }
+
+    if( n <= 0 || n > 1E5){
+        throw RuntimeException("max number of rows and columns are 1x10E5")
+    }
+
+    if( k < 0 || k > 1E5){
+        throw RuntimeException("max number of obstacles are 1x10E5")
+    }
+
+    for (i in 0..n + 1) {
+        allObstacles.add(Pair(0, i))
+        allObstacles.add(Pair(n + 1, i))
+        allObstacles.add(Pair(i, 0))
+        allObstacles.add(Pair(i, n + 1))
+    }
+
+    val queen = Pair(r_q, c_q)
+
+    val left = findDistance(allObstacles, queen, Pair(-1, 0))
+    val right = findDistance(allObstacles, queen, Pair(1, 0))
+    val bottom = findDistance(allObstacles, queen, Pair(0, -1))
+    val top = findDistance(allObstacles, queen, Pair(0, 1))
+    val leftTop = findDistance(allObstacles, queen, Pair(-1, 1))
+    val rightTop = findDistance(allObstacles, queen, Pair(1, 1))
+    val leftBottom = findDistance(allObstacles, queen, Pair(-1, -1))
+    val rightBottom = findDistance(allObstacles, queen, Pair(1, -1))
+
+    return top + bottom + left + right + leftTop + rightTop + leftBottom + rightBottom
+}
+
+fun findDistance(wallObstacles: Set<Pair<Int, Int>>, queen: Pair<Int, Int>, delta: Pair<Int, Int>): Int {
+    var distance = 0
+
+    var point = queen
+
+    while (true) {
+        point = Pair(point.first + delta.first, point.second + delta.second)
+        if (wallObstacles.contains(point)) {
+            return distance
+        } else distance++
+    }
 }
 
 fun main(args: Array<String>) {
--- queens_attack/src/test/kotlin/Tests.kt
+++ queens_attack/src/test/kotlin/Tests.kt
@@ -1,5 +1,6 @@
 import hernanbosqued.samples.queensAttack
 import org.junit.After
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 
@@ -16,7 +17,32 @@ class Tests {
     }
 
     @Test
-    fun test1() {
-        queensAttack(4, 0, 0, 4, emptyArray())
+    fun `4x4, queen in (4,4) and no obstacles`() {
+        val distance = queensAttack(4, 0, 4, 4, emptyArray())
+        Assert.assertEquals(9, distance)
+    }
+
+    @Test
+    fun `4x4, queen in (4,4) and one obstacle in (1,1)`() {
+        val distance = queensAttack(4, 1, 4, 4, arrayOf(arrayOf(1, 1)))
+        Assert.assertEquals(8, distance)
+    }
+
+    @Test
+    fun `5x5, queen in (4,3) and three obstacles in (1,1)`() {
+        val distance = queensAttack(5, 3, 4, 3, arrayOf(arrayOf(5, 5), arrayOf(4, 2), arrayOf(2, 3)))
+        Assert.assertEquals(10, distance)
+    }
+
+    @Test
+    fun `8x8, queen in (1,1) and no obstacles`() {
+        val distance = queensAttack(8, 0, 1, 1, emptyArray())
+        Assert.assertEquals(21, distance)
+    }
+
+    @Test
+    fun `100000x100000, queen in (4187,5068) and no obstacles`() {
+        val distance = queensAttack(100000, 0, 4187, 5068, emptyArray())
+        Assert.assertEquals(308369, distance)
     }
 }
